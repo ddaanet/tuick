690d19e0f5867bfd76ca88423461730546870fc6
David Allouche <david@ddaa.net>
2025-11-19 15:04:59 +0100

    🐛 Expose version in tuick.__version__

--- src/tuick/__init__.py
+++ src/tuick/__init__.py
@@ -4,3 +4,7 @@ Tuick is a wrapper for compilers and checkers that integrates with fzf and your
 text editor to provide fluid, keyboard-friendly, access to code error
 locations.
 """
+
+from importlib.metadata import version
+
+__version__ = version("tuick")
